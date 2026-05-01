21ba4b6b06f8e02cd676acab24c4041df0dc41af
vetlelimbo <vetle.werner03@gmail.com>
2026-05-01 13:26:55 +0200

    Create flashcard struct and flashcard data structure

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Flashcard {
+  char question[100];
+  char answer[200];
+} flashcard;
+
+void add_flashcard(flashcard flashcards[], flashcard *card) {
+  static int current_flashcard = 0;
+  flashcards[current_flashcard] = *card;
+}
+
+void print_flashcards(const flashcard* flashcards) {
+  for (int i = 0; i < 10; i++) {
+    printf("Question: %s\nAnswer: %s\n", 
+        flashcards[i].question,
+        flashcards[i].answer);
+  }
+}
 
 int main() {
-  printf("Hello World");
-  return 0;
+  flashcard flashcards[10];
+
+  flashcard flashcard1 = {
+    .question = "What is the capitol of france?",
+    .answer = "Paris"
+  };
+
+  add_flashcard(flashcards, &flashcard1);
+  print_flashcards(flashcards);
+  
+  return EXIT_SUCCESS;
 }
